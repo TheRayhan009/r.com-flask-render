7317185fd3ea1950f9b285de15e56ede5006ae4c
rayhan <156523736+TheRayhan009@users.noreply.github.com>
2024-04-04 12:17:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
+def home():
     return render_template("index.html")
+@app.route('/photos')
+def photo():
+    return render_template("photos.html")
+
+app.run(debug=True)
